a88572b81f5ccfa9900f932febedc4a5bb33e1f4
beolene1 <beolene@gmail.com>
2025-03-20 22:44:48 +0900

    chore: gitignore 추가

---
+++ week1Practice/week1Practice/ImageViewExample.swift
@@ -0,0 +1,18 @@
+//
+//  ImageViewExample.swift
+//  week1Practice
+//
+//  Created by tokkislove on 3/20/25.
+//
+
+import SwiftUI
+
+struct ImageViewExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageViewExample()
+}
