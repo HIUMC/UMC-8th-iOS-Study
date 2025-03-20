a22b58d8b6e4a3a00e582e9742bb3d757c0fe1ee
jiwookim1202 <godblessjiwoo@gmail.com>
2025-03-20 19:19:43 +0900

    UMC iOS week1_Starbucks

---
+++ Starbucks_App_/Starbucks_App_Tests/Starbucks_App_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Starbucks_App_Tests.swift
+//  Starbucks_App_Tests
+//
+//  Created by 김지우 on 3/20/25.
+//
+
+import Testing
+@testable import Starbucks_App_
+
+struct Starbucks_App_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Starbucks_App_/Starbucks_App_UITests/Starbucks_App_UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Starbucks_App_UITestsLaunchTests.swift
+//  Starbucks_App_UITests
+//
+//  Created by 김지우 on 3/20/25.
+//
+
+import XCTest
+
+final class Starbucks_App_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
