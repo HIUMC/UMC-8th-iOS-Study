988bc6019017ce435cc2a65bf2ba90f0d886716e
이서현 <iseohyeon@iseohyeon-ui-MacBookAir.local>
2025-03-23 03:08:12 +0900

    로그인 버튼이구나..

--- UMC_Starbucks/UMC_Starbucks/Views/LoginView.swift
+++ UMC_Starbucks/UMC_Starbucks/Views/LoginView.swift
@@ -4,6 +4,8 @@ import SwiftUI
 struct LoginView: View {
     var body: some View {
         mainTitleGroup
+        idPwdGroup
+        loginGroup
     }
 }
 
@@ -12,18 +14,61 @@ private var mainTitleGroup: some View {
     VStack(alignment: .leading) {
         Text("안녕하세요.")
             .font(.PretendardExtraBold24)
+            .kerning(1.2)
         Text("스타벅스입니다.")
             .font(.PretendardExtraBold24)
+            .kerning(1.2)
+        
+        Spacer().frame(height: 19)
         
         Text("회원 서비스 이용을 위해 로그인 해주세요")
             .font(.PretendardMedium16)
             .foregroundStyle(Color.loginGray)
         
         
+    }
+}
+
+//아이디 입력 및 비밀번호 입력
+private var idPwdGroup: some View {
+    VStack(alignment: .leading) {
+        Text("아이디")
+            .font(.PretendardRegular13)
+            .foregroundStyle(Color.idPwdBlack)
         
+        Spacer().frame(height: 1.98)
+        
+        Divider()
+        
+        Spacer().frame(height: 49.04)
+        
+        Text("비밀번호")
+            .font(.PretendardRegular13)
+            .foregroundStyle(Color.idPwdBlack)
+        
+        Spacer().frame(height: 1.98)
+        
+    Divider()
     }
     
 }
+
+private var loginGroup: some View {
+    VStack(alignment: .center) {
+        Text("로그인하기")
+            .font(.PretendardMedium16)
+            .foregroundStyle(Color.white)
+            .padding() //해야 하나?
+            .overlay(content: {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.loginButtonGreen)
+                    .frame(width: 402, height: 46)
+            })
+    }
+}
+
+
+
 
 
 
