1fe61be61979d3b5c05db4893923e59d43d10130
이가원 <igawon@igawon-ui-MacBookAir.local>
2025-03-24 01:16:31 +0900

    Add new folder

---
+++ STARBUCKS/Starbucks/Starbucks/StarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksApp.swift
+//  Starbucks
+//
+//  Created by 이가원 on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ STARBUCKS/Starbucks/StarbucksTests/StarbucksTests.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksTests.swift
+//  StarbucksTests
+//
+//  Created by 이가원 on 3/24/25.
+//
+
+import Testing
+@testable import Starbucks
+
+struct StarbucksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
