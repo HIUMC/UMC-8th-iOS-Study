e53e595e3355840a6682689891f02a9c5876f0a1
이서현 <iseohyeon@iseohyeon-ui-MacBookAir.local>
2025-03-24 00:37:52 +0900

    실습2주차 이미지 삽입

---
+++ week2/2st_Practice/2st_Practice/MovieView.swift
@@ -0,0 +1,24 @@
+//
+//  MovieView.swift
+//  2st_Practice
+//
+//  Created by 이서현 on 3/24/25.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MovieView()
+}
---
+++ week2/2st_Practice/2st_Practice/_st_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _st_PracticeApp.swift
+//  2st_Practice
+//
+//  Created by 이서현 on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct _st_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MovieView()
+        }
+    }
+}
