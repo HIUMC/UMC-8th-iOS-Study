1ebc4c4a12fdbc0f87c83cab3c7e3ba8601d2251
daehcjf@gmail.com <daehcjf@gmail.com>
2025-03-24 21:34:23 +0900

    ha..

---
+++ Starbucks_Week1_gogo/Starbucks_Week1_gogo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Starbucks_Week1_gogo
+//
+//  Created by 고석현 on 3/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
