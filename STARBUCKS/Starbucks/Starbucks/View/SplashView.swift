29630b3c49124e51573641821ce33d5173f7c3e6
이가원 <igawon@igawon-ui-MacBookAir.local>
2025-03-24 11:30:48 +0900

    1주차 과제 완료

--- STARBUCKS/Starbucks/Starbucks/View/SplashView.swift
+++ STARBUCKS/Starbucks/Starbucks/View/SplashView.swift
@@ -5,3 +5,17 @@
 //  Created by 이가원 on 3/24/25.
 //
 
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .fill(Color.green1)
+            Image(.starbucks)
+        }
+    }
+}
+#Preview {
+    SplashView()
+}
