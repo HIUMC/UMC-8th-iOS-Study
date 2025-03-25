a7f94fc4003639f2127e887d82643ea1313411b0
신민정 <sinminjeong@sinminjeongs-MacBook-Pro.local>
2025-03-25 18:47:46 +0900

    1주차

---
+++ oat/Practice2/Practice2/Practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice2App.swift
+//  Practice2
+//
+//  Created by 신민정 on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
