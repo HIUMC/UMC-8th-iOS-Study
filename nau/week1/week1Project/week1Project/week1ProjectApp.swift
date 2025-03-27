2161af79a685e7e127e317020bc7ca0e20cc0d0e
finger9999 <161580182+finger9999@users.noreply.github.com>
2025-03-27 22:17:26 +0900

    week1 실습 파일 추가

---
+++ nau/week1/week1Project/week1Project/week1ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  week1ProjectApp.swift
+//  week1Project
+//
+//  Created by nau on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct week1ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TicketView()
+        }
+    }
+}
