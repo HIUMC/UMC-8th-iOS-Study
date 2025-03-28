64f274b42d753fec7cdf8468ff8a2a040e8c525e
yys-63 <yangys0630@naver.com>
2025-03-28 21:27:24 +0900

    1주차 실습 완료

---
+++ yoony/week1/1st_Practice/1st_Practice/_st_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _st_PracticeApp.swift
+//  1st_Practice
+//
+//  Created by Yoonseo on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct _st_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
