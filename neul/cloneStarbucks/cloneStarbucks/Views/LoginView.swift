1ec7993cd8951d601ce6be45e752c61e5451d403
beolene1 <beolene@gmail.com>
2025-03-29 18:53:21 +0900

    week2: textfield 반영

--- neul/cloneStarbucks/cloneStarbucks/Views/LoginView.swift
+++ neul/cloneStarbucks/cloneStarbucks/Views/LoginView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct LoginView: View {
+    @ObservedObject var viewModel: LoginViewModel = .init()
+    @FocusState private var isFocused: Bool
+    
     var body: some View {
         VStack {
             topView
@@ -21,114 +24,109 @@ struct LoginView: View {
                 .padding(.bottom, 60)
         }
     }
-}
-
-private var topView: some View {
-    HStack {
-        VStack(alignment: .leading) {
-            Image("logo")
-                .resizable()
-                .frame(width: 97, height: 95)
-            Spacer().frame(height: 28)
-            Text("안녕하세요. \n스타벅스입니다.")
-                .foregroundStyle(.black)
-                .font(.MainTextExtraBold24)
-                .kerning(2)
-                .padding(.bottom, 10)
-            Text("회원 서비스 이용을 위해 로그인 해주세요")
-                .foregroundStyle(.gray01)
-                .font(.MainTextMedium16)
-                .kerning(-1)
-                
-        }
-        .padding(.leading, 20)
-        Spacer()
-    }
     
-}
-
-private var middleView: some View {
-    let labels = ["아이디", "비밀번호"]
-    return VStack(alignment: .center, spacing: 47) {
-        ForEach(labels, id: \.self) { label in
+    private var topView: some View {
+        HStack {
             VStack(alignment: .leading) {
-                Text(label)
-                    .font(.MainTextRegular13)
-                    .foregroundStyle(.black01)
-                    .padding(.leading, 20)
-                Divider()
-                    .frame(height: 0.7)
-                    .overlay(.gray00)
-                    .padding([.horizontal], 20)
-            }
-        }
-        Button(action:  {
-            print("로그인 버튼 클릭됨")
-        }, label: {
-            Text("로그인하기")
-                .foregroundStyle(.white)
-                .font(.MainTextMedium16)
-                .frame(maxWidth: .infinity, minHeight: 46)
-                .background(.green01)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-        })
-        .padding([.horizontal], 20)
-    }
-}
-
-private var bottomView: some View {
-    VStack {
-        Button(action: {
-            print("이메일로 회원가입하기 눌림")
-        }, label: {
-            Text("이메일로 회원가입하기")
-                .font(.MainTextRegular12)
-                .foregroundStyle(.gray04)
-                .underline(true, color: .gray04)
-        })
-        
-        Spacer()
-        
-        Button(action: {
-            print("카카오 로그인 클릭됨")
-        }) {
-            HStack {
-                Image("kakaoLogo")
-                    .padding(.leading, 20)
-                Spacer()
-            }
-            .overlay(alignment: .center, content: {
-                Text("카카오 로그인")
-                    .foregroundStyle(.black.opacity(0.85))
+                Image("logo")
+                    .resizable()
+                    .frame(width: 97, height: 95)
+                Spacer().frame(height: 28)
+                Text("안녕하세요. \n스타벅스입니다.")
+                    .foregroundStyle(.black)
+                    .font(.MainTextExtraBold24)
+                    .kerning(2)
+                    .padding(.bottom, 10)
+                Text("회원 서비스 이용을 위해 로그인 해주세요")
+                    .foregroundStyle(.gray01)
                     .font(.MainTextMedium16)
-            })
-            .frame(width: 306, height: 45)
-            .background(Color(hex: "FEE500"))
-            .clipShape(RoundedRectangle(cornerRadius: 5))
+                    .kerning(-1)
+                    
+            }
+            .padding(.leading, 20)
+            Spacer()
         }
         
-        Spacer()
+    }
+
+    private var middleView: some View {
         
-        Button(action: {
-            print("애플 로그인 클릭됨")
-        }) {
-            HStack {
-                Image("appleLogo")
-                    .padding(.leading, 20)
-                Spacer()
+        let fields: [(label: String, binding: Binding<String>)] = [("아이디", $viewModel.id), ("비밀번호", $viewModel.pwd)]
+        return VStack(alignment: .center, spacing: 47) {
+            ForEach(fields, id: \.label) { field in
+                CustomUnderlineTextField(placeholder: field.label, text: field.binding)
             }
-            .overlay(alignment: .center, content: {
-                Text("Apple로 로그인")
+            Button(action:  {
+                print("로그인 버튼 클릭됨")
+            }, label: {
+                Text("로그인하기")
                     .foregroundStyle(.white)
                     .font(.MainTextMedium16)
+                    .frame(maxWidth: .infinity, minHeight: 46)
+                    .background(.green01)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
             })
-            .frame(width: 306, height: 45)
-            .background(.black)
-            .clipShape(RoundedRectangle(cornerRadius: 5))
+            
         }
+        .padding([.horizontal], 20)
     }
-    
+
+    private var bottomView: some View {
+        VStack {
+            Button(action: {
+                print("이메일로 회원가입하기 눌림")
+            }, label: {
+                Text("이메일로 회원가입하기")
+                    .font(.MainTextRegular12)
+                    .foregroundStyle(.gray04)
+                    .underline(true, color: .gray04)
+            })
+            
+            Spacer()
+            
+            Button(action: {
+                print("카카오 로그인 클릭됨")
+            }) {
+                HStack {
+                    Image("kakaoLogo")
+                        .padding(.leading, 20)
+                    Spacer()
+                }
+                .overlay(alignment: .center, content: {
+                    Text("카카오 로그인")
+                        .foregroundStyle(.black.opacity(0.85))
+                        .font(.MainTextMedium16)
+                })
+                .frame(width: 306, height: 45)
+                .background(Color(hex: "FEE500"))
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+            }
+            
+            Spacer()
+            
+            Button(action: {
+                print("애플 로그인 클릭됨")
+            }) {
+                HStack {
+                    Image("appleLogo")
+                        .padding(.leading, 20)
+                    Spacer()
+                }
+                .overlay(alignment: .center, content: {
+                    Text("Apple로 로그인")
+                        .foregroundStyle(.white)
+                        .font(.MainTextMedium16)
+                })
+                .frame(width: 306, height: 45)
+                .background(.black)
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+            }
+        }
+        
+    }
+
 }
+
 
 
 
