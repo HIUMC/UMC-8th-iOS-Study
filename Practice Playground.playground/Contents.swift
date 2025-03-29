e9820e661a057d3ddd410ebac6321d9725686c23
ggani_0526 <161475073+gaworii@users.noreply.github.com>
2025-03-29 12:18:59 +0900

    Revert "2주차과제완료"
    This reverts commit d609a3b783308e4456ff7710600462aa99368223.

--- Practice Playground.playground/Contents.swift
+++ Practice Playground.playground/Contents.swift
@@ -1,5 +1,3 @@
+import UIKit
 
-let name : String = "가니"
-let age : Int = 22
-let message : String = "이름: \(name), 나이: \(age)"
-
+var greeting = "Hello, playground"
---
+++ STARBUCKS/Starbucks/Starbucks/StarbucksApp.swift
@@ -0,0 +1,16 @@
+//
+//  StarbucksApp.swift
+//  Starbucks
+//
+//  Created by 이가원 on 3/24/25.
+//
+import SwiftUI
+
+@main
+struct StarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
