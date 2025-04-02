4c58821c1666f2cd7f1b397fb036c98e53339340
gogo <daehcjf@gmail.com>
2025-04-02 16:24:03 +0900

    파일 정리함

--- gogo/StarbucksApp/StarbucksApp/ViewModels/SignupViewModel.swift
+++ gogo/StarbucksApp/StarbucksApp/ViewModels/SignupViewModel.swift
@@ -5,28 +5,25 @@
 ////  Created by 고석현 on 4/1/25.
 ////
 //
-//import Foundation
-//import SwiftUI
-//
-//class SignupViewModel: ObservableObject {
-//    
-//    @AppStorage("nickname") private var nickname : String = ""
-//    @AppStorage("email") private var email : String = ""
-//    @AppStorage("password") private var password : String = ""
-//    
-//   
-//    
-//    @Published var signupModel: SignupModel = .init(nickname: "", email: "", password: "")
-//    
-//    public func saveStorage(){
-//        self.nickname = signupModel.nickname
-//        self.email = signupModel.email
-//        self.password = signupModel.password
-//        print("AppStorage에 저장됌.")
-//    }
-//    
-//}
-//
-//#Preview {
-//    SignupView(viewModel: SignupViewModel())
-//}
+import Foundation
+import SwiftUI
+
+class SignupViewModel: ObservableObject {
+    
+    @AppStorage("nickname") private var nickname : String = ""
+    @AppStorage("email") private var email : String = ""
+    @AppStorage("password") private var password : String = ""
+    
+   
+    
+    @Published var signupModel: SignupModel = .init(nickname: "", email: "", password: "")
+    
+    public func saveStorage(){
+        self.nickname = signupModel.nickname
+        self.email = signupModel.email
+        self.password = signupModel.password
+        print("AppStorage에 저장됌.")
+    }
+    
+}
+
