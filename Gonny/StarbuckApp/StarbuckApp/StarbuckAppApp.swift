8cb10947dbce201c1eed0dcd37efeab6a0aa7178
Byeongseon-Park <parkbsbsbsbsbs@gmail.com>
2025-04-07 02:55:31 +0900

    3주차과제

--- Gonny/StarbuckApp/StarbuckApp/StarbuckAppApp.swift
+++ Gonny/StarbuckApp/StarbuckApp/StarbuckAppApp.swift
@@ -7,11 +7,26 @@
 
 import SwiftUI
 
-@main
+/*@main
 struct StarbuckAppApp: App {
     var body: some Scene {
         WindowGroup {
             SplashView()//앱이 실행될 때 가장 먼저 보여줄 View
+        }
+    }
+}
+ */
+@main
+struct YourAppNameApp: App {
+    @AppStorage("isLoggedIn") var isLoggedIn = false
+
+    var body: some Scene {
+        WindowGroup {
+            if isLoggedIn {
+                MyTabView() // 홈 탭이 포함된 메인 화면
+            } else {
+                LoginView(userInfo: LoginViewModel())
+            }
         }
     }
 }
---
+++ Gonny/StarbuckApp/Views/RecommendMenuView.swift
@@ -0,0 +1,7 @@
+//
+//  RecommendView.swift
+//  StarbuckApp
+//
+//  Created by 박병선 on 4/3/25.
+//
+
