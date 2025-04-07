aec45c23428e5c41b270fd3f2f510b721e47ca43
beolene1 <beolene@gmail.com>
2025-04-07 16:21:00 +0900

    week4: 문법 워크북

---
+++ neul/week4/week4Practice/week4Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  week4Practice
+//
+//  Created by tokkislove on 4/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
