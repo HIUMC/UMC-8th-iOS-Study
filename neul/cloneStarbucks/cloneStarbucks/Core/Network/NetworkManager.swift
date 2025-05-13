e83b6e2359f91dd7488c478e121e2e5d671fe236
beolene1 <beolene@gmail.com>
2025-05-13 17:30:07 +0900

    [week6] RESTT API 로그인 구현

--- neul/cloneStarbucks/cloneStarbucks/Core/Network/NetworkManager.swift
+++ neul/cloneStarbucks/cloneStarbucks/Core/Network/NetworkManager.swift
@@ -20,12 +20,11 @@ import SwiftUI
 final class NetworkManager {
     
     static let shared = NetworkManager()
+    let keychain = KeychainService.shared
         
     private let session: Session
     
-   // let redirectURI = "kakaob0e106cc098023e4d9bd784349293d69://oauth"
     
-    private let urlString: String = "https://kauth.kakao.com/oauth/authorize?client_id=\(Config.restApiKey)&redirect_uri=http://localhost:8080/oauth2/redirect/kakao&response_type=code"
     
     init() {
         let configuration = URLSessionConfiguration.default
@@ -36,6 +35,8 @@ final class NetworkManager {
     
     // MARK: - GET 요청
     func kakaoLogin() async -> URL? {
+        let urlString: String = "https://kauth.kakao.com/oauth/authorize?client_id=\(Config.restApiKey)&redirect_uri=http://kakao\(Config.nativeAppKey)//oauth&response_type=code"
+        
         
         do {
             /*let response = try await AF.request(urlString, method: .get, encoding: URLEncoding.default)
@@ -50,6 +51,64 @@ final class NetworkManager {
             return nil
         }*/
     }
+    
+    
+    func handleRedirect(url: URL) async {
+        guard let components = URLComponents(url: url, resolvingAgainstBaseURL: true),
+                let queryItems = components.queryItems else {
+            print("❌ URL 파싱 실패")
+            return
+        }
+        if let code = queryItems.first(where: { $0.name == "code" })?.value {
+            print("✅ Authorization Code:", code)
+            await requestKakaoToken(code: code)
+        } else if let error = queryItems.first(where: { $0.name == "error" })?.value {
+            let errorDescription = queryItems.first(where: { $0.name == "error_description" })?.value
+            print("❌ 인증 실패:", error, "-", errorDescription ?? "")
+        }
+    }
+    
+    func requestKakaoToken(code: String) async {
+        let urlString = "https://kauth.kakao.com/oauth/token"
+        
+        let parameters: [String: String] = [
+            "grant_type": "authorization_code",
+            "client_id": Config.restApiKey,
+            "redirect_uri": "http://kakao\(Config.nativeAppKey)//oauth",  // 네 앱 설정에 따라 변경
+            "code": code
+        ]
+
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/x-www-form-urlencoded;charset=utf-8"
+        ]
+        
+        do {
+            let response = try await AF.request(urlString, method: .post, parameters: parameters, encoder: URLEncodedFormParameterEncoder.default, headers: headers)
+                .serializingDecodable(KakaoTokenResponse.self)
+                .value
+            print("POST 성공:")
+            if let tokenInfo = extractToken(from: response) {
+                keychain.saveToken(tokenInfo)
+            }
+        } catch {
+            print("POST 실패:", error.localizedDescription)
+        }
+    }
+    
+    func extractToken(from token: KakaoTokenResponse?) -> TokenInfo? {
+        guard let token = token else {
+            print("토큰이 없습니다.")
+            return nil
+        }
+        
+        let tokenInfo = TokenInfo(
+            accessToken: token.accessToken,
+            refreshToken: token.refreshToken
+        )
+        
+        return tokenInfo
+    }
+    
     /*
     // MARK: - POST 요청
     func postUser(user: UserDTO) async {
